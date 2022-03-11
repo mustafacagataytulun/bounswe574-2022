02486e21441e7da72ef9c49a9a8d7813a54b13f3
Mustafa Çağatay Tulun <mustafatulun@gmail.com>
2022-03-11 17:59:31 +0300

    Added new field for cover image. #17

--- gamesiplayed/games/models.py
+++ gamesiplayed/games/models.py
@@ -1,8 +1,9 @@
 from django.db import models
 
 class Game(models.Model):
-    game_title = models.CharField(max_length=250)
-    wikidata_item_identifier = models.CharField(max_length=50)
+    game_title = models.CharField('Title', max_length=250)
+    wikidata_item_identifier = models.CharField('Wikidata Item Identifier', max_length=50)
+    cover_image_uri = models.CharField('Cover Image URI', max_length=1024, null=True)
 
     def __str__(self):
         return self.game_title
