aba3198501f1157240f5ebcc6835e338cc5166ef
Mustafa Çağatay Tulun <mustafatulun@gmail.com>
2022-03-11 18:07:12 +0300

    Added game completion date to order games. #17

--- gamesiplayed/games/models.py
+++ gamesiplayed/games/models.py
@@ -4,6 +4,7 @@ class Game(models.Model):
     game_title = models.CharField('Title', max_length=250)
     wikidata_item_identifier = models.CharField('Wikidata Item Identifier', max_length=50)
     cover_image_uri = models.CharField('Cover Image URI', max_length=1024, null=True)
+    completion_date = models.DateTimeField('Completion Date')
 
     def __str__(self):
         return self.game_title
