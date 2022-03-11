a56e8899a03860cddb2bb4dde146ce6bee66163c
Mustafa Çağatay Tulun <mustafatulun@gmail.com>
2022-03-11 15:17:46 +0300

    Created Game model. #16

--- gamesiplayed/games/models.py
+++ gamesiplayed/games/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Game(models.Model):
+    game_title = models.CharField(max_length=250)
+    wikidata_item_identifier = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.game_title
