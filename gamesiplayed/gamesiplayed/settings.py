7be9fde9c333c82d67415a528d2a121e5499dac6
Mustafa Çağatay Tulun <mustafatulun@gmail.com>
2022-03-13 01:46:17 +0300

    Updated database config for deployment. #20

--- gamesiplayed/gamesiplayed/settings.py
+++ gamesiplayed/gamesiplayed/settings.py
@@ -10,6 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
+import os
+
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -76,18 +78,30 @@ WSGI_APPLICATION = 'gamesiplayed.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'gamesiplayed',
-        'USER': 'postgres',
-        'HOST': 'localhost',
-        'PORT': 5432,
-        'OPTIONS': {
-            'passfile': '.pgpass',
-        },
+if 'RDS_DB_NAME' in os.environ:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': os.environ['RDS_DB_NAME'],
+            'USER': os.environ['RDS_USERNAME'],
+            'PASSWORD': os.environ['RDS_PASSWORD'],
+            'HOST': os.environ['RDS_HOSTNAME'],
+            'PORT': os.environ['RDS_PORT'],
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': 'gamesiplayed',
+            'USER': 'postgres',
+            'HOST': 'localhost',
+            'PORT': 5432,
+            'OPTIONS': {
+                'passfile': '.pgpass',
+            },
+        }
+    }
 
 
 # Password validation
