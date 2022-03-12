22a1570111716d27a566386a02b353ad575fbfcb
Mustafa Çağatay Tulun <mustafatulun@gmail.com>
2022-03-12 14:49:59 +0300

    Created game detail page with dummy data. #18

--- gamesiplayed/games/urls.py
+++ gamesiplayed/games/urls.py
@@ -5,4 +5,5 @@ from . import views
 app_name = 'games'
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
+    path('<int:pk>/', views.DetailView.as_view(), name='detail'),
 ]
--- gamesiplayed/games/views.py
+++ gamesiplayed/games/views.py
@@ -4,3 +4,14 @@ from games.models import Game
 
 class IndexView(generic.ListView):
     model = Game
+
+class DetailView(generic.DetailView):
+    model = Game
+    extra_context = {
+        'game_data': {
+            'description': 'This is a test description.',
+            'genre': 'Test genre, another test genre',
+            'developer': 'Lorem Ipsum Developer',
+            'publisher': 'Dolor Sit Amet Publisher'
+        }
+    }
