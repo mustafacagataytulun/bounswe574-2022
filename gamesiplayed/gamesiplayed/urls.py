b8b4e7224aaf36c6c6d49653e2e15598ae3cd1bf
Mustafa Çağatay Tulun <mustafatulun@gmail.com>
2022-03-13 03:21:31 +0300

    Redirected root URL to games app. #20

--- gamesiplayed/gamesiplayed/urls.py
+++ gamesiplayed/gamesiplayed/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('games/', include('games.urls')),
+    path('', RedirectView.as_view(pattern_name='games:index')),
 ]
