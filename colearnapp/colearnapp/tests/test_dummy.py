ad94e1c38a66afbc519854841c93b7c163215dff
Mustafa Çağatay Tulun <mustafatulun@gmail.com>
2022-04-12 02:24:41 +0300

    Added a dummy test to integrate with CircleCI. #48

---
+++ colearnapp/colearnapp/tests/test_dummy.py
@@ -0,0 +1,9 @@
+from unittest import TestCase
+
+class DummyTests(TestCase):
+    def test_is_ci_pipeline_working(self):
+        """
+        Is CI pipeline working properly? This test should be deleted
+        after creating real tests.
+        """
+        self.assertTrue(True)
