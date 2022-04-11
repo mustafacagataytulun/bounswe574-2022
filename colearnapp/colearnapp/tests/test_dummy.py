523b4b71d1da59f2bfcd794fd2b84cf690c40e5f
Mustafa Çağatay Tulun <mustafatulun@gmail.com>
2022-04-12 02:35:10 +0300

    Fixed linting errors. #48

--- colearnapp/colearnapp/tests/test_dummy.py
+++ colearnapp/colearnapp/tests/test_dummy.py
@@ -1,9 +1,15 @@
+"""
+Test module of ColearnApp project.
+"""
 from unittest import TestCase
 
 class DummyTests(TestCase):
+    """
+    A class containing dummy tests to integrate with CI pipeline.
+    """
     def test_is_ci_pipeline_working(self):
         """
         Is CI pipeline working properly? This test should be deleted
         after creating real tests.
         """
-        self.assertTrue(True)
+        self.assertEqual(True, True)
