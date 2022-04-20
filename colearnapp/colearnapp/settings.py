dc7e176d7e05046e1612bea12f1958e787bb0395
Mustafa Çağatay Tulun <mustafatulun@gmail.com>
2022-04-20 04:46:49 +0300

    Fixed password reset template. #29

--- colearnapp/colearnapp/settings.py
+++ colearnapp/colearnapp/settings.py
@@ -35,13 +35,13 @@ ALLOWED_HOSTS = [
 # Application definition
 
 INSTALLED_APPS = [
+    'users',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'users',
 ]
 
 MIDDLEWARE = [
