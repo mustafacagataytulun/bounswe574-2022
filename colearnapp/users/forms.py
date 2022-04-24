d0be141773fbba00a9647c5eaf79eb18bd5d8d7c
Mustafa Çağatay Tulun <mustafatulun@gmail.com>
2022-04-25 02:17:51 +0300

    Fixed User Manager issue. #28

--- colearnapp/users/forms.py
+++ colearnapp/users/forms.py
@@ -1,3 +1,4 @@
+from django.contrib.auth import get_user_model
 from django.contrib.auth.forms import UserChangeForm, UserCreationForm
 
 from .models import ColearnAppUser
@@ -5,9 +6,11 @@ from .models import ColearnAppUser
 class CustomUserCreationForm(UserCreationForm):
     class Meta(UserCreationForm.Meta):
         model = ColearnAppUser
+        User = get_user_model()
         fields = ('email', 'username', 'password1', 'password2', 'interests',)
 
 class CustomUserChangeForm(UserChangeForm):
     class Meta(UserChangeForm.Meta):
         model = ColearnAppUser
+        User = get_user_model()
         fields = ('email', 'username', 'interests',)
