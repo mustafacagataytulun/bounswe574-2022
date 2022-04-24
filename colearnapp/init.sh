3fc613750a48cc1c9753ca75b7f04318cdaa2f2b
Mustafa Çağatay Tulun <mustafatulun@gmail.com>
2022-04-25 02:30:54 +0300

    Fixed application start issue with Docker. #48

--- colearnapp/init.sh
+++ colearnapp/init.sh
@@ -5,4 +5,4 @@ email=admin@example.com
 password=pass
 
 python3 manage.py migrate
-echo "from django.contrib.auth.models import User; User.objects.create_superuser('$user', '$email', '$password')" | python3 manage.py shell
+echo "from django.contrib.auth import get_user_model; from django.contrib.auth.models import User; User = get_user_model(); User.objects.create_superuser('$user', '$email', '$password')" | python3 manage.py shell
