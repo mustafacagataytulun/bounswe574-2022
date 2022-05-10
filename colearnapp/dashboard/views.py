2e4a55b118447fff46357a2421cfa532893854ca
Mustafa Çağatay Tulun <mustafatulun@gmail.com>
2022-05-10 17:13:51 +0300

    Removed trailing white-space. #32

--- colearnapp/dashboard/views.py
+++ colearnapp/dashboard/views.py
@@ -7,7 +7,7 @@ def index(request):
 
     if request.user.is_authenticated:
         own_spaces = Space.objects.filter(created_by=request.user).order_by('-id')[:10]
-    
+
     all_spaces = Space.objects.order_by('-id')[:10]
 
     return render(request, "dashboard/index.html", {
