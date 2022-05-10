5b77846c6c8f6eefd0128e2cd70aace1fd5d2dbc
Mustafa Çağatay Tulun <mustafatulun@gmail.com>
2022-05-10 17:12:15 +0300

    Added space links to homepage. #32

--- colearnapp/dashboard/views.py
+++ colearnapp/dashboard/views.py
@@ -1,4 +1,16 @@
 from django.shortcuts import render
 
+from spaces.models import Space
+
 def index(request):
-    return render(request, "dashboard/index.html")
+    own_spaces = None
+
+    if request.user.is_authenticated:
+        own_spaces = Space.objects.filter(created_by=request.user).order_by('-id')[:10]
+    
+    all_spaces = Space.objects.order_by('-id')[:10]
+
+    return render(request, "dashboard/index.html", {
+        'all_spaces':all_spaces,
+        'own_spaces':own_spaces
+        })
