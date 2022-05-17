c4f90ea33aea2631457b61d349c1deea8a59d5fe
Mustafa Çağatay Tulun <mustafatulun@gmail.com>
2022-05-17 16:04:57 +0300

    Added tests for space creation. #34

---
+++ colearnapp/users/utils.py
@@ -0,0 +1,20 @@
+import random
+import string
+
+from django.test import TestCase
+
+from users.models import ColearnAppUser
+
+def create_random_string(char_count):
+       return ''.join(random.choice(string.ascii_letters) for _ in range(char_count))
+
+def create_test_user():
+    email = create_random_string(10) + "@example.com"
+    username = create_random_string(10)
+    password = create_random_string(20)
+    interests = create_random_string(10)
+    return (ColearnAppUser.objects.create_user(email=email, username=username, password=password, interests=interests), password,)
+
+def create_and_login_test_user(test_case: TestCase):
+    (user, real_password,) = create_test_user()
+    test_case.client.login(username=user.email, password=real_password)
