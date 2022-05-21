1e5525c8108b20da67fb5afb638b21b18b0f3b2c
Mustafa Çağatay Tulun <mustafatulun@gmail.com>
2022-05-21 14:59:32 +0300

    Implemented pagination for space list in home screen. #56

--- colearnapp/dashboard/views.py
+++ colearnapp/dashboard/views.py
@@ -1,3 +1,4 @@
+from django.core.paginator import Paginator
 from django.db.models import Q
 from django.shortcuts import render
 
@@ -9,19 +10,23 @@ def index(request):
     own_spaces = None
 
     if request.user.is_authenticated:
-        own_spaces = Space.objects.filter(created_by=request.user).order_by('-id')[:10]
+        own_spaces = Space.objects.filter(created_by=request.user).order_by('-id')
 
     form = SpaceSearchForm(request.GET or None)
     is_searched = form.is_valid() and len(form.cleaned_data['search'])
 
     if is_searched:
-        all_spaces = Space.objects.filter(Q(name__icontains=form.cleaned_data['search']) | Q(tags__icontains=form.cleaned_data['search'])).order_by('-id')[:10]
+        all_spaces = Space.objects.all().filter(Q(name__icontains=form.cleaned_data['search']) | Q(tags__icontains=form.cleaned_data['search'])).order_by('-id')
     else:
-        all_spaces = Space.objects.order_by('-id')[:10]
+        all_spaces = Space.objects.all().order_by('-id')
+
+    paginator = Paginator(all_spaces, per_page=24)
+    page = paginator.get_page(request.GET.get('page', 1))
 
     return render(request, "dashboard/index.html", {
         'all_spaces':all_spaces,
         'own_spaces':own_spaces,
         'form':form,
         'is_searched':is_searched,
+        'page':page,
         })
