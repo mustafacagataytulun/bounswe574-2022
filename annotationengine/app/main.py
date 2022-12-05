14264647e5ea3f8be1fbd7076f734fdb65eed786
Mustafa Çağatay Tulun <mustafatulun@gmail.com>
2022-12-05 14:43:22 +0300

    Added support for specifying containers. #23

--- annotationengine/app/main.py
+++ annotationengine/app/main.py
@@ -16,23 +16,23 @@ def get_annotations():
         "http://www.w3.org/ns/ldp.jsonld"
     ]}
 
-@app.post("/annotations/",
+@app.post("/{collection_id}/",
     status_code=status.HTTP_201_CREATED,
     response_class=JsonLdResponse,
     dependencies=[
         Depends(MediaType.application_ld_json),
         Depends(WebAnnotationDataModel.context_json_ld),
         Depends(WebAnnotationDataModel.type_annotation),])
-async def post_annotation(request: Request, response: Response):
+async def post_annotation(collection_id: str, request: Request, response: Response):
     incoming_request = await request.json()
     incoming_request['created'] = datetime.utcnow().isoformat()
     client = MongoClient('mongodb://' + os.getenv('DB_USER') + ':' + os.getenv('DB_PASSWORD') + '@' + os.getenv('DB_HOST') + ':' + os.getenv('DB_PORT') + '/')
     db = client[os.getenv('DB_NAME')]
-    annotations_db_collection = db['annotations']
+    annotations_db_collection = db[collection_id]
     annotations_db_collection.insert_one(incoming_request)
 
     id = incoming_request.pop('_id')
-    incoming_request['id'] = os.getenv('BASE_URL') + str(id)
+    incoming_request['id'] = os.getenv('BASE_URL') + collection_id + '/' + str(id)
 
     response.headers['Allow'] = 'PUT,GET,OPTIONS,HEAD,DELETE,PATCH'
     response.headers['Location'] = incoming_request['id']
