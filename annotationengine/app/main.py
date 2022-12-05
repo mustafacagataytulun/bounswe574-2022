0d06e7551078e2927017f3c6727289f0201aa883
Mustafa Çağatay Tulun <mustafatulun@gmail.com>
2022-12-05 15:32:33 +0300

    Added support for container retrieval. #23

--- annotationengine/app/main.py
+++ annotationengine/app/main.py
@@ -9,12 +9,49 @@ from app.web_annotation_data_model import WebAnnotationDataModel
 
 app = FastAPI(title="Annotations API")
 
-@app.get("/annotations/")
-def get_annotations():
-    return {"@context": [
-        "http://www.w3.org/ns/anno.jsonld",
-        "http://www.w3.org/ns/ldp.jsonld"
-    ]}
+@app.get("/{collection_id}/",
+    response_class=JsonLdResponse,
+    dependencies=[Depends(MediaType.application_ld_json)])
+def get_annotations(collection_id: str, page: int = None):
+    client = MongoClient('mongodb://' + os.getenv('DB_USER') + ':' + os.getenv('DB_PASSWORD') + '@' + os.getenv('DB_HOST') + ':' + os.getenv('DB_PORT') + '/')
+    db = client[os.getenv('DB_NAME')]
+    annotations_db_collection = db[collection_id]
+    total_count = annotations_db_collection.count_documents({})
+
+    if page is None:
+        items = []
+        cursor = annotations_db_collection.find(skip=0, limit=10)
+
+        for annotation in cursor:
+            items.append({
+                "id": os.getenv('BASE_URL') + collection_id + '/' + str(annotation['_id']),
+                "type": "Annotation",
+                "body": annotation['body'],
+                "target": annotation['target'],
+            })
+
+        response_model = {
+                "@context": [
+                    "http://www.w3.org/ns/anno.jsonld",
+                    "http://www.w3.org/ns/ldp.jsonld"
+                    ],
+                "id": os.getenv('BASE_URL') + collection_id + '/',
+                "type": ["BasicContainer", "AnnotationCollection"],
+                "total": total_count,
+                "first": {
+                    "id": os.getenv('BASE_URL') + collection_id + '/?page=0',
+                    "type": "AnnotationPage",
+                }
+                    }
+        
+        if total_count > 10:
+            response_model['first']['next'] = os.getenv('BASE_URL') + collection_id + '/?page=1'
+
+        response_model['first']['items'] = items
+        response_model['last'] = os.getenv('BASE_URL') + collection_id + '/?page=' + str(int((total_count - 1) / 10))
+    
+
+    return response_model
 
 @app.post("/{collection_id}/",
     status_code=status.HTTP_201_CREATED,
