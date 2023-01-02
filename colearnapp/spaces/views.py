87eff7f1625e658df4aaa9db50f5d4dd4ad974bb
Mustafa Çağatay Tulun <mustafatulun@gmail.com>
2023-01-02 10:11:43 +0300

    Added missing requirements. #13

--- colearnapp/spaces/views.py
+++ colearnapp/spaces/views.py
@@ -1,6 +1,6 @@
 import os
 import logging
-import requests # pylint: disable=import-error
+import requests
 
 from django.contrib.auth.decorators import login_required
 from django.http import JsonResponse
